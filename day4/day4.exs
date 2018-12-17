9b4ce04daea72e3d9a5c86f4eaa0898422d2c042
Leo B <leo.the.soundmonster@gmail.com>
2018-12-17 13:14:49 +0100

    Complete day4

--- day4/day4.exs
+++ day4/day4.exs
@@ -5,21 +5,6 @@ require IEx
 defmodule Shift do
   defstruct [:guard, :date, :asleep]
 
-
-  def new({{:guard, id, date, _time}, events}) do
-    %Shift{guard: id, date: date, asleep: unfold(events)}
-  end
-
-  def unfold([{:asleep, _, minute_from}, {:awake, _, minute_to}]) do
-    for minute <- 0..59 do
-      minute >= minute_from && minute < minute_to
-    end
-  end
-
-  def unfold(_do_recursion_here) do
-    []
-  end
-
   def parse_line(<<"[", date :: binary - size(10), " ", time :: binary - size(5), "] Guard #", rest::binary>>) do
     id = rest |> String.split |> hd
     {:guard, id, date, time}
@@ -37,22 +22,64 @@ defmodule Shift do
     :error
   end
 
-  def chunk(log) do
-    log
-    |> Enum.chunk_by(fn x -> match?({:guard, _, _, _}, x) end)
-    |> pairwise_fold
+  def parse_logs(input) do
+    input
+    |> Enum.reduce([], &do_parse_logs/2)
+    |> Enum.reverse
+    |> Enum.map(&minutes_to_list/1)
   end
 
-  def pairwise_fold([first, second | rest]) do
-    [{first, second}] ++ pairwise_fold(rest)
+  def aggregate_by_elf(shifts) do
+    shifts
+    |> Enum.group_by(fn shift -> shift.guard end, fn shift -> shift.asleep end)
+    |> Enum.map(fn {guard, asleeps} ->
+      {guard, Enum.reduce(asleeps, empty_minutes_list(), &sum_shifts/2)}
+    end)
   end
 
-  def pairwise_fold([first]) do
-    [{first, nil}]
+  defp sum_shifts(left, right) do
+    left
+    |> Enum.zip(right)
+    |> Enum.map(fn {l, r} -> l + r end)
   end
 
-  def pairwise_fold([]) do
-    []
+  defp do_parse_logs(line, acc) do
+    case line do
+      {:guard, id, date, _time} ->
+        [%Shift{guard: id, date: date, asleep: empty_minutes_map()} | acc]
+      {:asleep, date, from} ->
+        shift = hd(acc)
+        [
+           %Shift{shift | date: date, asleep: set_asleep(shift.asleep, from)}
+           | tl(acc)
+        ]
+      {:awake, date, from} ->
+        shift = hd(acc)
+        [
+           %Shift{shift | date: date, asleep: set_awake(shift.asleep, from)}
+           | tl(acc)
+        ]
+    end
+  end
+
+  def empty_minutes_map() do
+    Enum.reduce(0..59, %{}, fn minute, map -> Map.put(map, minute, 0) end)
+  end
+
+  def empty_minutes_list() do
+    List.duplicate(0, 60)
+  end
+
+  def set_asleep(minutes, from) do
+    Enum.reduce(from..59, minutes, & Map.put(&2, &1, 1))
+  end
+
+  def set_awake(minutes, from) do
+    Enum.reduce(from..59, minutes, & Map.put(&2, &1, 0))
+  end
+
+  def minutes_to_list(%Shift{asleep: minutes} = shift) do
+    %Shift{shift | asleep: Enum.map(0..59, & Map.get(minutes, &1, 0))}
   end
 end
 
@@ -63,12 +90,41 @@ defmodule Day4 do
     |> String.split("\n")
     |> Enum.sort()
     |> Enum.reject(& &1 == "")
+    |> Enum.map(&Shift.parse_line/1)
+  end
+
+  def puzzle1 do
+    elf_stats = input()
+    |> Shift.parse_logs
+    |> Shift.aggregate_by_elf
+
+    {id, asleep} = Enum.max_by(elf_stats, fn {_, arr} -> Enum.sum(arr) end)
+
+    winner_id = String.to_integer(id)
+    {_, winner_minute} = asleep
+    |> Enum.with_index()
+    |> Enum.max_by(fn {score, _minute} -> score end)
+
+    IO.inspect(winner_id * winner_minute, label: :puzzle1)
+  end
+
+  def puzzle2 do
+    elf_stats = input()
+    |> Shift.parse_logs
+    |> Shift.aggregate_by_elf
+
+    {winner_id, winner_minute, _} = elf_stats
+    |> Enum.map(fn {elf, asleep} ->
+      asleep
+      |> Enum.with_index()
+      |> Enum.map(fn {asleep?, minute} -> {String.to_integer(elf), minute, asleep?} end)
+    end)
+    |> List.flatten()
+    |> Enum.max_by(& elem(&1, 2))
+
+    IO.inspect(winner_id * winner_minute, label: :puzzle1)
   end
 end
 
-Day4.input
-  |> Enum.map(&Shift.parse_line/1)
-  |> Shift.chunk()
-  |> Enum.map(&Shift.new/1)
-  |> IO.inspect
-
+Day4.puzzle1
+Day4.puzzle2
