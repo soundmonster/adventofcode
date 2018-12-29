aed10c77585c3b6a7265db38f75e45f52bfba4c4
Leo B <leo.the.soundmonster@gmail.com>
2018-12-30 00:55:06 +0100

    WiP day 9

--- day09/day9.exs
+++ day09/day9.exs
@@ -3,7 +3,7 @@
 defmodule Game do
   defstruct [
     :circle,
-    :turns_played,
+    :turn,
     :next_marble,
     :players,
     :current_player,
@@ -14,67 +14,96 @@ defmodule Game do
   def new(players) when players > 0 do
     %Game{
       circle: [0],
-      turns_played: 0,
+      turn: -1,
       players: players,
-      current_player: 0,
+      current_player: "-",
       current_marble_index: 0,
       next_marble: 1,
       scores: %{}
     }
   end
 
-  def turn(%Game{turns_played: 0, players: players} = game) do
+  def turn(%Game{turn: -1, players: players} = game) do
     %Game{
       game
       | circle: [0, 1],
-        turns_played: 1,
+        turn: 0,
         current_marble_index: 1,
         current_player: 1,
         next_marble: 2
     }
   end
 
-  def turn(%Game{turns_played: 1, players: players} = game) do
+  def turn(%Game{turn: 0, players: players} = game) do
     %Game{
       game
       | circle: [0, 2, 1],
-        turns_played: 2,
+        turn: 1,
         current_marble_index: 1,
-        current_player: rem(2, players),
+        current_player: rem(1, players) + 1,
         next_marble: 3
     }
   end
 
-  def turn(%Game{turns_played: 2, players: players} = game) do
+  def turn(%Game{turn: 1, players: players} = game) do
     %Game{
       game
       | circle: [0, 2, 1, 3],
-        turns_played: 3,
+        turn: 2,
         current_marble_index: 3,
-        current_player: rem(3, players),
+        current_player: rem(2, players) + 1,
         next_marble: 4
     }
   end
 
   def turn(%{next_marble: m} = game) when rem(m, 23) != 0 do
-    next_marble_index = rem(game.current_marble_index + 2, length(game.circle))
-    {l, r} = Enum.split(game.circle, next_marble_index)
-    circle = l ++ [m] ++ r
-    turns_played = game.turns_played + 1
+    next_marble_index =
+      clockwise(
+        length(game.circle),
+        game.current_marble_index,
+        2
+      )
+
+    circle = List.insert_at(game.circle, next_marble_index, m)
+    turn = game.turn + 1
+    current_player = rem(turn, game.players) + 1
 
     %Game{
       game
       | next_marble: m + 1,
-        turns_played: turns_played,
+        turn: turn,
         current_marble_index: next_marble_index,
         circle: circle,
-        current_player: rem(turns_played, game.players)
+        current_player: current_player
     }
   end
 
   def turn(%{next_marble: m} = game) when rem(m, 23) == 0 do
-    # update scores
-    game
+    next_marble_index =
+      counterclockwise(
+        length(game.circle),
+        game.current_marble_index,
+        7
+      )
+
+    {score_marble, circle} = List.pop_at(game.circle, next_marble_index)
+    turn = game.turn + 1
+    current_player = rem(turn, game.players) + 1
+    scores = update_scores(game.scores, current_player, m + score_marble)
+
+    %Game{
+      game
+      | next_marble: m + 1,
+        turn: turn,
+        current_marble_index: next_marble_index,
+        circle: circle,
+        current_player: current_player,
+        scores: scores
+    }
+  end
+
+  def update_scores(scores, player, increment) do
+    Map.update(scores, player, increment, &(&1 + increment))
   end
 
   def inspect(game) do
@@ -91,6 +120,26 @@ defmodule Game do
 
     game
   end
+
+  def clockwise(total, from, offset) do
+    next = from + offset
+
+    if next == total do
+      next
+    else
+      rem(next, total)
+    end
+  end
+
+  def counterclockwise(total, from, offset) do
+    next = from - offset
+
+    if next < 0 do
+      total - next
+    else
+      next
+    end
+  end
 end
 
 defmodule Day9 do
@@ -98,8 +147,8 @@ defmodule Day9 do
   end
 
   def puzzle1 do
-    0..30
-    |> Enum.reduce(Game.new(10), fn _, game ->
+    0..300
+    |> Enum.reduce(Game.new(9), fn _, game ->
       game
       |> Game.inspect()
       |> Game.turn()
